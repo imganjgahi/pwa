2cd8d5e4535edd5e75fbbd17481f00f3b2b804d1
Mehran Ganjgahi <imganjgahi@gmail.com>
2021-09-15 18:12:45 +0430

    Updates

--- sw.js
+++ sw.js
@@ -3,34 +3,33 @@ console.log(prefix, " is working ...")
 
 let cacheData = "appV1"
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(cacheData)
-        .then(cach => {
-            cach.addAll([
-                "https://imganjgahi.github.io/pwa",
-                "https://imganjgahi.github.io/pwa/#/",
-                "https://mehrapp.netlify.app",
-                "https://mehrapp.netlify.app/",
-                "/pwa/static/js/main.chunk.js",
-                "/pwa/static/js/vendors~main.chunk.js",
-                "/pwa/static/css/vendors~main.chunk.css",
-                "/pwa/static/js/bundle.js", 
-                "/pwa/index.html",
-                "/pwa/",
-            ])
-        })
-        .catch(err => console.log("ERR: ", err))
+            .then(cach => {
+                cach.addAll([
+                    "/static/js/main.chunk.js",
+                    "/static/js/vendors~main.chunk.js",
+                    "/static/css/vendors~main.chunk.css",
+                    "/static/js/bundle.js",
+                    "/favicon.ico",
+                    "/logo192.png",
+                    "/logo512.png",
+                    "/index.html",
+                    "/"
+                ])
+            })
+            .catch(err => console.log("ERR: ", err))
     )
 })
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request)
-        .then(res => {
-            if(res) return res
-            else return fetch(event.request)
-        })
-        .catch(err => console.log("ERR: ", err))
+            .then(res => {
+                if (res) return res
+                else return fetch(event.request)
+            })
+            .catch(err =>  console.log("ERR: ", err) )
     )
 })
