9166d7f70adc06903caac2419f844f1379f29a50
Mehran Ganjgahi <imganjgahi@gmail.com>
2021-09-15 18:27:41 +0430

    localLink

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
     return (
         <div>
             <h1>MEHR APP</h1>
             <p>The Home Page</p>
+            <p> <Link to="https://10.0.2.2:8080"> Local Link </Link> </p>
         </div>
     )
 }
