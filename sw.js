02f8e5b5e162957940fdc4823f0d9c0553b3b0cb
Mehran Ganjgahi <imganjgahi@gmail.com>
2021-09-18 17:59:14 +0430

    Updates

--- sw.js
+++ sw.js
@@ -4,23 +4,23 @@ console.log(prefix, " is working ...")
 let cacheData = "appV1"
 
 self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(cacheData)
-            .then(cach => {
-                cach.addAll([
-                    // "/static/js/main.chunk.js",
-                    // "/static/js/vendors~main.chunk.js",
-                    // "/static/css/vendors~main.chunk.css",
-                    // "/static/js/bundle.js",
-                    // "/favicon.ico",
-                    // "/logo192.png",
-                    // "/logo512.png",
-                    "/index.html",
-                    "/"
-                ])
-            })
-            .catch(err => console.log("ERR: ", err))
-    )
+    // event.waitUntil(
+    //     caches.open(cacheData)
+    //         .then(cach => {
+    //             cach.addAll([
+    //                 // "/static/js/main.chunk.js",
+    //                 // "/static/js/vendors~main.chunk.js",
+    //                 // "/static/css/vendors~main.chunk.css",
+    //                 // "/static/js/bundle.js",
+    //                 // "/favicon.ico",
+    //                 // "/logo192.png",
+    //                 // "/logo512.png",
+    //                 // "/pwa/index.html",
+    //                 // "/"
+    //             ])
+    //         })
+    //         .catch(err => console.log("ERR: ", err))
+    // )
 })
 
 self.addEventListener('fetch', function (event) {
@@ -33,6 +33,7 @@ self.addEventListener('fetch', function (event) {
                 else {
                     return fetch(event.request)
                     .then(fethRes => {
+                        console.log("FETCH: ", event.request.url)
                         return caches.open("dynamic")
                         .then(cach => {
                             cach.put(event.request.url, fethRes.clone())
