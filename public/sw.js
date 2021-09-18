cb7f0ef907394cf0a79153d58fcb13a7f508970b
Mehran Ganjgahi <imganjgahi@gmail.com>
2021-09-18 17:57:58 +0430

    stop caching static

--- public/sw.js
+++ public/sw.js
@@ -4,23 +4,23 @@ console.log(prefix, " is working ...")
 let cacheData = "appV1"
 
 self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(cacheData)
-            .then(cach => {
-                cach.addAll([
-                    // "/static/js/main.chunk.js",
-                    // "/static/js/vendors~main.chunk.js",
-                    // "/static/css/vendors~main.chunk.css",
-                    // "/static/js/bundle.js",
-                    // "/favicon.ico",
-                    // "/logo192.png",
-                    // "/logo512.png",
-                    "/index.html",
-                    "/"
-                ])
-            })
-            .catch(err => console.log("ERR: ", err))
-    )
+    // event.waitUntil(
+    //     caches.open(cacheData)
+    //         .then(cach => {
+    //             cach.addAll([
+    //                 // "/static/js/main.chunk.js",
+    //                 // "/static/js/vendors~main.chunk.js",
+    //                 // "/static/css/vendors~main.chunk.css",
+    //                 // "/static/js/bundle.js",
+    //                 // "/favicon.ico",
+    //                 // "/logo192.png",
+    //                 // "/logo512.png",
+    //                 // "/pwa/index.html",
+    //                 // "/"
+    //             ])
+    //         })
+    //         .catch(err => console.log("ERR: ", err))
+    // )
 })
 
 self.addEventListener('fetch', function (event) {
