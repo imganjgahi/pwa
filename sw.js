3ba7a0c937938d3fe78087fe5539e9e5750fd430
Mehran Ganjgahi <imganjgahi@gmail.com>
2021-09-21 15:04:44 +0430

    Updates

--- sw.js
+++ sw.js
@@ -2,46 +2,65 @@ const prefix = "[ SERVICE WORKER ] "
 console.log(prefix, " is working ...")
 
 let cacheData = "appV1"
+const CanCache = false
 
 self.addEventListener('install', function (event) {
-    // event.waitUntil(
-    //     caches.open(cacheData)
-    //         .then(cach => {
-    //             cach.addAll([
-    //                 // "/static/js/main.chunk.js",
-    //                 // "/static/js/vendors~main.chunk.js",
-    //                 // "/static/css/vendors~main.chunk.css",
-    //                 // "/static/js/bundle.js",
-    //                 // "/favicon.ico",
-    //                 // "/logo192.png",
-    //                 // "/logo512.png",
-    //                 // "/pwa/index.html",
-    //                 // "/"
-    //             ])
-    //         })
-    //         .catch(err => console.log("ERR: ", err))
-    // )
+    console.log(prefix, "is install . . .")
+    if(!CanCache) return
+    event.waitUntil(
+        caches.open(cacheData)
+            .then(cach => {
+                cach.addAll([
+                    "/index.html",
+                    "/offline.html",
+                ])
+            })
+            .catch(err => console.log("ERR: ", err))
+    )
+})
+self.addEventListener('activate', function (event) {
+    console.log(prefix, "is Actived . . .")
+    event.waitUntil(caches.keys()
+        .then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== cacheData && key !== "dynamic") {
+                        console.log(prefix, "Remove Cache", key)
+                        return caches.delete(key)
+                    }
+                }))
+        })
+        .catch(err => console.log("ERR: ", err)))
 })
 
 self.addEventListener('fetch', function (event) {
+    if(!CanCache) return event.respondWith(fetch(event.request))
     event.respondWith(
         caches.match(event.request)
             .then(res => {
                 if (res) {
                     return res
-                } 
+                }
                 else {
                     return fetch(event.request)
-                    .then(fethRes => {
-                        console.log("FETCH: ", event.request.url)
-                        return caches.open("dynamic")
-                        .then(cach => {
-                            cach.put(event.request.url, fethRes.clone())
-                            return fethRes
+                        .then(fethRes => {
+                            console.log(prefix, " ADD URL: ", event.request.url)
+                            return caches.open("dynamic")
+                                .then(cach => {
+                                    cach.put(event.request.url, fethRes.clone())
+                                    return fethRes
+                                })
                         })
-                    })
-                } 
+                        .catch(err => {
+                            console.log("ERR: ", err)
+                            return caches.open(cacheData)
+                                .then(cach => {
+                                    console.log("return to ofline page ")
+                                    return cach.match("/offline.html")
+                                })
+                        })
+                }
             })
-            .catch(err =>  console.log("ERR: ", err) )
+            .catch(err => console.log("ERR: ", err))
     )
 })
